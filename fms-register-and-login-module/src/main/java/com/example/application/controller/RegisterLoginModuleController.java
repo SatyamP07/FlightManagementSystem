b9f9961fea15bd9eab4e6000834d9d5387cc4fc4
Satyam Pisal <37584330+SatyamP07@users.noreply.github.com>
2020-09-16 16:55:51 +0530

    commit by Satyam (controller added)

--- fms-register-and-login-module/src/main/java/com/example/application/controller/RegisterLoginModuleController.java
+++ fms-register-and-login-module/src/main/java/com/example/application/controller/RegisterLoginModuleController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.application.entities.User;
+import com.example.application.model.Credentials;
 import com.example.application.model.UserInfo;
 import com.example.application.service.UserService;
 
@@ -56,5 +57,13 @@ public class RegisterLoginModuleController {
 		return ResponseEntity.ok("User " + userName + "deleted successfully.");
 	}
 	
+	@GetMapping("/authenticate/user")
+	public ResponseEntity<UserInfo> authnticateUser(@RequestBody Credentials credentials) {
+		return ResponseEntity.ok(userService.authenticateUser(credentials));
+	}
 	
+	@GetMapping("/authenticate/admin")
+	public ResponseEntity<UserInfo> authnticateAdmin(@RequestBody Credentials credentials) {
+		return ResponseEntity.ok(userService.authenticateUser(credentials));
+	}
 }
--- fms-register-and-login-module/src/main/java/com/example/application/utility/Encryption.java
+++ fms-register-and-login-module/src/main/java/com/example/application/utility/Encryption.java
@@ -4,7 +4,7 @@ public class Encryption {
 
 	public static String passwordEncryptor(String password) {
 		int key = 7;
-		int encryptedNumber = 0;
+		long encryptedNumber = 0;
 		for(int i = 0; i < password.length(); i++) {
 			encryptedNumber += (int)password.charAt(i) * key;
 		}
